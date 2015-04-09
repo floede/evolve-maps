249ba9d885f03e54f1aec1a1ba7a9e6095b751e4
Janus Hasseriis <janus@hasseriis.net>
2015-04-09 12:23:36 +0200

    Map sets

--- evolve-maps.js
+++ evolve-maps.js
@@ -1,19 +1,20 @@
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    maps: [
-      { title: "Armory", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/ARMORY2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Aviary", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/AVIARY2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Barracks", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/BARRACKS2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "The Dam", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/DAM2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Distillery", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/DISTILLERY2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Fusion Plant", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/FUSIONPLANT2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Med Lab", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/MEDLAB2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Orbital Drill", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/ORBITALDRILL2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Rendering Plant", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/RENDERINGPLANT2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Refueling Tower", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/REFUELINGTOWER2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Weather Control", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/WEATHERCONTROL2.png", imgSource: "http://www.evolvehype.com" },
-      { title: "Wraith Trap", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/WRAITHTRAP2.png", imgSource: "http://www.evolvehype.com" }
-    ]
+      set: 
+        { name: "Evolve Hype Callout Maps", keyword: [ "callouts", "names"], source: "http://www.evolvehype.com", maps: [
+            { title: "Armory", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/ARMORY2.png" },
+            { title: "Aviary", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/AVIARY2.png" },
+            { title: "Barracks", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/BARRACKS2.png" },
+            { title: "The Dam", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/DAM2.png" },
+            { title: "Distillery", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/DISTILLERY2.png" },
+            { title: "Fusion Plant", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/FUSIONPLANT2.png" },
+            { title: "Med Lab", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/MEDLAB2.png" },
+            { title: "Orbital Drill", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/ORBITALDRILL2.png" },
+            { title: "Rendering Plant", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/RENDERINGPLANT2.png" },
+            { title: "Refueling Tower", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/REFUELINGTOWER2.png" },
+            { title: "Weather Control", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/WEATHERCONTROL2.png" },
+            { title: "Wraith Trap", imgUrl: "http://evolvehype.com/wp-content/uploads/2015/02/WRAITHTRAP2.png" }
+        ]}
   });
 }
